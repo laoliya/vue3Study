a3b630ffb7aa69194d5d13a5cfe85118cf500ad7
lizhiyong <936366286@qq.com>
2021-12-02 17:05:04 +0800

    fix: vuex

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -3,15 +3,22 @@
     <div @click="add">
         {{count}}
     </div>
+    <button @click="asyncAdd">asyncAdd</button>
 </template>
 
 <script setup>
 import { computed } from 'vue'
-import { useStore } from '../store/gvuex'
+// import { useStore } from '../store/gvuex' 手写mini-vuex
+import { useStore } from 'vuex'
 let store = useStore()
 let count = computed(() => store.state.count)
 
+let double = computed(() => store.getters.double)
+console.log(double)
 function add() {
     store.commit('add')
+}
+function asyncAdd() {
+    store.dispatch('asyncAdd')
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,28 @@
-import { createStore } from "./gvuex";
+// import { createStore } from "./gvuex"; // 手写的mini-vuex
 
+import { createStore } from "vuex";
 const store = createStore({
   state() {
     return {
       count: 666,
     };
   },
+  getters: {
+    double(state) {
+      return state.count * 2;
+    },
+  },
   mutations: {
     add(state) {
       state.count++;
     },
   },
+  actions: {
+    asyncAdd({ commit }) {
+      setTimeout(() => {
+        commit("add");
+      }, 1000);
+    },
+  },
 });
-export default store
+export default store;
